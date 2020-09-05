0e8e732cbd437e4e59064bbd346d12157474647a
jjdcn <shzu_csy@163.com>
2020-09-06 03:02:03 +0800

    测试git版本 modify

--- src/main/java/cn/jjdcn/design/proxy/TestClass.java
+++ src/main/java/cn/jjdcn/design/proxy/TestClass.java
@@ -3,6 +3,6 @@ package cn.jjdcn.design.proxy;
 public class TestClass {
 
     public static void main(String[] args) {
-        System.out.println("edit in online");
+        System.out.println("modify in local");
     }
 }
