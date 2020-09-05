cb0da3d7d745702dfcc44fbfff41aa1e8fe5960a
csy0320 <924951774@qq.com>
2020-09-06 02:58:15 +0800

    Update TestClass.java
    在线修改

--- src/main/java/cn/jjdcn/design/proxy/TestClass.java
+++ src/main/java/cn/jjdcn/design/proxy/TestClass.java
@@ -3,6 +3,6 @@ package cn.jjdcn.design.proxy;
 public class TestClass {
 
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("edit in online");
     }
 }
